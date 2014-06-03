55b7438f2e7a32797e82376ac7f69f6e8a7f24ea
Jason Chinsen <onzyone@gmail.com>
2014-06-03 19:57:16 -0400

    refactors main to a class and moved obd2 items into their own module.

--- obd2link/obd2_link.py
+++ obd2link/obd2_link.py
@@ -1,125 +1,133 @@
 import time
 from datetime import datetime
 
-import obd2_connection
-import obd2_constants
 import core.file_io as file_io
+import core.dict_helper as dict_helper
 import accelerometer.adxl345 as accelerometer
+import obd2.obd2_connection as obd2_connection
+import obd2.obd2_constants as obd2_constants
 
 
-def get_connection():
+class Obd2Link():
 
-# this will need to be taken care of by a get call later on
-    port = '/dev/ttyUSB0'
-    baudrate = 115200
+    def __init__(self):
+        self.connection = self.get_connection()
+        self.conn = obd2_connection.Obd2Connection()
+        self.ph = file_io.PropertiesHelper()
+        self.dh = dict_helper.DictHelper()
 
-    conn = obd2_connection.Obd2Connection()
-    connection = conn.obd2_connection(port=port, baudrate=baudrate)
+    def get_connection(self):
 
-    return connection
+    # this will need to be taken care of by a get call later on
+        port = '/dev/ttyUSB0'
+        baudrate = 115200
 
-def get_constants():
-    constants = obd2_constants.CONSTANTS
-    for key, value in constants.items():
-        print key, value
+        conn = obd2_connection.Obd2Connection()
+        connection = conn.obd2_connection(port=port, baudrate=baudrate)
 
-def get_sensors(connection):
+        return connection
 
-    temp_dict = {'time stamp': str(datetime.now())}
+    def get_constants(self):
+        constants = obd2_constants.CONSTANTS
+        for key, value in constants.items():
+            print key, value
 
-    conn = obd2_connection.Obd2Connection()
-    conn.obd2_close(connection)
-    conn.obd2_open(connection)
+    def get_sensors(self):
 
-    sensors = obd2_constants.SENSORS
-    ph = file_io.PropertiesHelper()
-    for key, value in sensors.items():
-#        print k, v
-        for key, value in value.items():
-            if key == 'hex':
-                conn.obd2_write(connection, value)
-                read = conn.obd2_read(connection)
-                temp_dict.update({value: read})
-                print 'after reading bus: ' + read
-            else:
-                print key, value
+        temp_dict = {'time stamp': str(datetime.now())}
 
-    print temp_dict
-    ph.write_csv(temp_dict)
+        self.conn.obd2_close(self.connection)
+        self.conn.obd2_open(self.connection)
 
-def get_version(connection):
+        sensors = obd2_constants.SENSORS
 
-    conn = obd2_connection.Obd2Connection()
-    conn.obd2_close(connection)
-    conn.obd2_open(connection)
-    conn.obd2_is_open(connection)
-
-    conn.obd2_write(connection, 'ATI')
-    read = conn.obd2_read(connection)
-
-    print 'read after ATI: ' + read
-
-def odb2_innitialize(connection):
+        for key, value in sensors.items():
+    #        print k, v
+            for key, value in value.items():
+                if key == 'hex':
+                    self.conn.obd2_write(self.connection, value)
+                    read = self.conn.obd2_read(self.connection)
+                    temp_dict.update({value: read})
+                    print 'after reading bus: ' + read
+                else:
+                    print key, value
 
 
-    # this is still in prototype stage too.
-    count = 0
-    conn = obd2_connection.Obd2Connection()
-    conn.obd2_close(connection)
-    conn.obd2_open(connection)
+        self.dh.sort_dict(temp_dict)
+        print temp_dict
 
-    conn.obd2_write(connection, 'ATZ')
-    read = conn.obd2_read(connection)
+        self.ph.write_csv(temp_dict)
 
-    print 'read after ATZ: ' + read
+    def get_version(self):
 
-    time.sleep(5)
+        self.conn.obd2_close(self.connection)
+        self.conn.obd2_open(self.connection)
+        self.conn.obd2_is_open(self.connection)
 
-    #echo off
-    conn.obd2_write(connection, 'ATE0')
-    read = conn.obd2_read(connection)
+        self.conn.obd2_write(self.connection, 'ATI')
+        read = self.conn.obd2_read(self.connection)
 
-    print 'read after ATE0: ' + read
+        print 'read after ATI: ' + read
 
-    conn.obd2_write(connection, 'ATDP')
-    read = conn.obd2_read(connection)
-
-    print 'read after ATDP: ' + read
-
-    #not connected: UNABLE TO CONNECTSEARCHING
+    def innitialize(self):
 
 
-    #Sensor("          Supported PIDs", "0100", hex_to_bitstring  ,""       ),
-    conn.obd2_write(connection, '0100')
-    time.sleep(5)
-    read = conn.obd2_read(connection)
-    print 'read after 0100: ' + read
+        # this is still in prototype stage too.
+        count = 0
 
-    #Sensor("     Coolant Temperature", "0105", temp              ,"C"      ),
-    conn.obd2_write(connection, '0105')
-    time.sleep(5)
-    read = conn.obd2_read(connection)
-    # expected output: hex, 41 05 79
-    # where 41 05 is the header and 79 is the hex value
-    print 'read after 0105: ' + read
+        self.conn.obd2_close(self.connection)
+        self.conn.obd2_open(self.connection)
 
-def get_acc_axes():
+        self.conn.obd2_write(self.connection, 'ATZ')
+        read = self.conn.obd2_read(self.connection)
 
-    acc = accelerometer.ADXL345()
-    axes = acc.getAxes(True)
+        print 'read after ATZ: ' + read
 
-    print "ADXL345 on address 0x%x:" % (acc.address)
-    print "   x = %.3fG" % ( axes['x'] )
-    print "   y = %.3fG" % ( axes['y'] )
-    print "   z = %.3fG" % ( axes['z'] )
+        time.sleep(5)
 
-def main():
-    connection = get_connection()
-    #get_version(connection)
-    odb2_innitialize(connection)
-    #get_constants()
-    get_sensors(connection)
-    get_acc_axes()
+        #echo off
+        self.conn.obd2_write(self.connection, 'ATE0')
+        read = self.conn.obd2_read(self.connection)
+
+        print 'read after ATE0: ' + read
+
+        self.conn.obd2_write(self.connection, 'ATDP')
+        read = self.conn.obd2_read(self.connection)
+
+        print 'read after ATDP: ' + read
+
+        #not connected: UNABLE TO CONNECTSEARCHING
+
+
+        #Sensor("          Supported PIDs", "0100", hex_to_bitstring  ,""       ),
+        self.conn.obd2_write(self.connection, '0100')
+        read = self.conn.obd2_read(self.connection)
+        print 'read after 0100: ' + read
+
+        #Sensor("     Coolant Temperature", "0105", temp              ,"C"      ),
+        self.conn.obd2_write(self.connection, '0105')
+        read = self.conn.obd2_read(self.connection)
+        # expected output: hex, 41 05 79
+        # where 41 05 is the header and 79 is the hex value
+        print 'read after 0105: ' + read
+
+    def get_acc_axes(self):
+
+        acc = accelerometer.ADXL345()
+        axes = acc.getAxes(True)
+
+        print "ADXL345 on address 0x%x:" % (acc.address)
+        print "   x = %.3fG" % ( axes['x'] )
+        print "   y = %.3fG" % ( axes['y'] )
+        print "   z = %.3fG" % ( axes['z'] )
+
+    def main(self):
+
+        #get_version(connection)
+        self.innitialize()
+        #get_constants()
+        self.get_sensors()
+        self.get_acc_axes()
 
 if __name__ == '__main__':
-    main()
+    Obd2Link().main()
