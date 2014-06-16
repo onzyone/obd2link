d73a50df2d197d677621efc6e820ca3088804184
Jason Chinsen <onzyone@gmail.com>
2014-06-15 23:04:53 -0400

    really ... now ...

--- obd2link/lcd/ssd1306.py
+++ obd2link/lcd/ssd1306.py
@@ -11,8 +11,8 @@ def set_lcd(message, position, clear):
     # 0, 8, 16, 24
     message = str(message)
     led = gaugette.ssd1306.SSD1306(reset_pin=RESET_PIN, dc_pin=DC_PIN)
-    print clear
     if clear:
+        print 'clearing? {0}'.format(clear)
         led.begin()
         led.clear_display()
 
